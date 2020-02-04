16dad1dff1c63c8b49632a98c526a9518469b833
Samuel Tijani <omalitijani@gmail.com>
2020-02-04 08:46:49 +0100

    modify to model and view

--- src/main/java/com/samtj/CoursesWebApp/CoursesController.java
+++ src/main/java/com/samtj/CoursesWebApp/CoursesController.java
@@ -5,10 +5,10 @@
  */
 package com.samtj.CoursesWebApp;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  *
@@ -18,10 +18,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class CoursesController {
     
     @RequestMapping("/courses")
-    public String courses(HttpServletRequest req) {
-        HttpSession session = req.getSession();
-        String cname = req.getParameter("cname");
-        session.setAttribute("cname", cname);
-        return "courses";
+    public ModelAndView courses(@RequestParam("cname") String coursename) {
+        ModelAndView mv = new ModelAndView();
+        mv.addObject("cname", coursename);
+        mv.setViewName("courses");
+        return mv;
     }
 }
